96b13c3840e8ca2e23cce4a7ebd63d0bc314b7b2
Fewing <yangzheyu98@gmail.com>
2021-03-05 19:21:58 +0800

    完成拍摄视频功能

--- app/src/main/java/com/fecostudio/faceguard/MainActivity.kt
+++ app/src/main/java/com/fecostudio/faceguard/MainActivity.kt
@@ -2,36 +2,59 @@ package com.fecostudio.faceguard
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Matrix
 import android.graphics.Paint
-import android.hardware.camera2.CameraCharacteristics
+import android.graphics.Rect
+import android.media.MediaCodec
+import android.media.MediaRecorder
+import android.media.MediaScannerConnection
 import android.os.Bundle
 import android.util.Log
 import android.util.Size
-import android.view.SurfaceHolder
-import android.view.SurfaceView
-import android.view.WindowManager
+import android.view.*
+import android.webkit.MimeTypeMap
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.fecostudio.faceguard.utils.*
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.face.FaceDetection
 import com.google.mlkit.vision.face.FaceDetectorOptions
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
         const val REQUEST_CODE_PERMISSIONS = 10
+        private const val RECORDER_VIDEO_BITRATE: Int = 10_000_000
+        private const val MIN_REQUIRED_RECORDING_TIME_MILLIS: Long = 1000L
+
+        /** Creates a [File] named with the current date and time */
+        private fun createFile(context: Context, extension: String): File {
+            val sdf = SimpleDateFormat("yyyy_MM_dd_HH_mm_ss_SSS", Locale.CHINA)
+            val appContext = context.applicationContext
+            val mediaDir = context.externalMediaDirs.firstOrNull()?.let {
+                File(it, appContext.resources.getString(R.string.app_name)).apply { mkdirs() } }
+            return File(mediaDir, "VID_${sdf.format(Date())}.$extension")
+        }
     }
-    private lateinit var surfaceView: AutoFitSurfaceView
+    private lateinit var surfaceView: SurfaceView
     private var cameraProvider: ProcessCameraProvider? = null
     private val executor = Executors.newSingleThreadExecutor()
+    /** File where the recording will be saved */
+    private val outputFile: File by lazy { createFile(this, "mp4") }
     private lateinit var converter: YuvToRgbConverter
     private var bitmap: Bitmap? = null
     private val detector = FaceDetection.getClient(
@@ -40,11 +63,19 @@ class MainActivity : AppCompatActivity() {
                     .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_NONE)
                     .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_NONE).enableTracking()
                     .build())
+    private lateinit var recorderSurface: Surface
+    private val recorder: MediaRecorder by lazy { createRecorder() }
+    private var isRecording = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.addFlags(WindowManager.LayoutParams.
-        FLAG_KEEP_SCREEN_ON);   //保持屏幕常亮
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);   //保持屏幕常亮
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                // Hide the nav bar and status bar
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)//设置全屏
         setContentView(R.layout.activity_main)
         // YuvToRgb converter.
         converter = YuvToRgbConverter(this)
@@ -63,21 +94,34 @@ class MainActivity : AppCompatActivity() {
 
                 // Selects appropriate preview size and configures view finder
                 Log.d("size", "Surface View size: ${surfaceView.width} x ${surfaceView.height}")
-                holder.setFixedSize(1080, 1080*surfaceView.height/surfaceView.width)
+                surfaceView.layoutParams.height = surfaceView.width * 16 / 9
+                holder.setFixedSize(1080, 1080 * 16 / 9)
 
                 // To ensure that size is set, initialize camera in the view's thread
                 //surfaceView.post { initializeCamera() }
             }
         })
         // Camera permission needed for CameraX.
-        requestPermissions(arrayOf(Manifest.permission.CAMERA), REQUEST_CODE_PERMISSIONS)
-
+        requestPermissions(arrayOf(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO), REQUEST_CODE_PERMISSIONS)
         // Init CameraX.
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
         cameraProviderFuture.addListener(Runnable {
             cameraProvider = cameraProviderFuture.get()
             startCameraIfReady()
         }, ContextCompat.getMainExecutor(this))
+    }
+
+    /** Creates a [MediaRecorder] instance using the provided [Surface] as input */
+    private fun createRecorder() = MediaRecorder().apply {
+        setAudioSource(MediaRecorder.AudioSource.MIC)
+        setVideoSource(MediaRecorder.VideoSource.SURFACE)
+        setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+        setOutputFile(outputFile.absolutePath)
+        setVideoEncodingBitRate(RECORDER_VIDEO_BITRATE)
+        setVideoFrameRate(25)
+        setVideoSize(1080, 1920)
+        setVideoEncoder(MediaRecorder.VideoEncoder.H264)
+        setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
     }
 
     @SuppressLint("UnsafeExperimentalUsageError")
@@ -88,37 +132,60 @@ class MainActivity : AppCompatActivity() {
         val imageAnalysis = ImageAnalysis.Builder().setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).setTargetResolution(
                 Size(1080, 1920)
         ).build()
-        imageAnalysis.setAnalyzer(executor, ImageAnalysis.Analyzer {
+        imageAnalysis.setAnalyzer(executor, {
             val start = System.currentTimeMillis()
             val mediaImage = it.image
             if (mediaImage != null) {
                 val image = InputImage.fromMediaImage(mediaImage, it.imageInfo.rotationDegrees)
-                val result = detector.process(image)
+                detector.process(image)
                         .addOnSuccessListener { faces ->
-                            var bitmap = allocateBitmapIfNecessary(it.width, it.height)
+                            val bitmap = allocateBitmapIfNecessary(it.width, it.height)
                             converter.yuvToRgb(it.image!!, bitmap)
                             it.close()
                             val paint = Paint()
-                            var matrix = Matrix()
+                            val matrix = Matrix()
                             matrix.postRotate(90f)
-                            matrix.postTranslate(bitmap.height.toFloat(),0f)
-                            val newCanvas = surfaceView.holder.lockHardwareCanvas()
-                            newCanvas.drawBitmap(bitmap, matrix,paint)
-                            for (face in faces){
-                                newCanvas.drawRect(face.boundingBox,paint)
+                            matrix.postTranslate(bitmap.height.toFloat(), 0f)
+                            val previewCanvas = surfaceView.holder.lockHardwareCanvas()//预览的画布
+                            if (isRecording) {
+                                val recordCanvas = recorderSurface.lockHardwareCanvas()//录制的画布
+                                recordCanvas.drawBitmap(bitmap, matrix, paint)
+                                for (face in faces) {
+                                    recordCanvas.drawRect(face.boundingBox, paint)
+                                }
+                                recordCanvas.save()
+                                recorderSurface.unlockCanvasAndPost(recordCanvas)
                             }
-                            newCanvas.save()
-                            surfaceView.holder.unlockCanvasAndPost(newCanvas)
+                            previewCanvas.drawBitmap(bitmap, matrix, paint)
+                            for (face in faces) {
+                                previewCanvas.drawRect(face.boundingBox, paint)
+                            }
+                            previewCanvas.save()
+                            surfaceView.holder.unlockCanvasAndPost(previewCanvas)
                             val delayTime = System.currentTimeMillis() - start
                             //Log.d("time", "startCameraIfReady: "+delayTime+"ms")
                         }
                         .addOnFailureListener { e ->
-                            // Task failed with an exception
-                            // ...
+                            Log.e("MLKit", "startCameraIfReady: "+e.localizedMessage, )
                         }
             }
         })
         cameraProvider!!.bindToLifecycle(this, CameraSelector.DEFAULT_BACK_CAMERA, imageAnalysis)
+    }
+
+    fun startRecord(view: View) {
+        if (isRecording) {
+            recorder.stop()
+            MediaScannerConnection.scanFile(
+                    view.context, arrayOf(outputFile.absolutePath), null, null)
+            isRecording = false
+        } else {
+            recorder.prepare()
+            recorder.start()
+            recorderSurface = recorder.surface
+            Log.d("record", "Recording started")
+            isRecording = true
+        }
     }
 
     private fun allocateBitmapIfNecessary(width: Int, height: Int): Bitmap {
@@ -136,6 +203,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun isPermissionsGranted(): Boolean {
-        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED){
+            return  true
+        }
+        return false
     }
 }
